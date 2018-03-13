4b8b641a17a1aac557a85ea30bedcc67b6e05cd2
Grega Pusnik <grega.pusnik@visionect.com>
2018-03-13 17:04:52 +0100

    Added gender fix.

--- IDaaS/GApps/pipeline.py
+++ IDaaS/GApps/pipeline.py
@@ -10,10 +10,13 @@ def get_avatar(request, backend, strategy, details, response,
         url = response['image'].get('url')
         ext = url.split('.')[-1]
         domain = response['domain']
-        gender = response['gender']
         user_id = response['id']
     if url:
         request.session['avatar_url'] = url.split('?')[0]+'?sz=200'
+    if response.get('gender'):
+        request.session['gender'] = response.get('gender')
+    else:
+        request.session['gender'] = "male"
     if domain and user_id:
         #check if user is admin   
         response = requests.get(
