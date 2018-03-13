f3b52299f9223470b8467ee258be89dbb66d1baa
Grega Pusnik <grega.pusnik@visionect.com>
2018-03-13 15:27:22 +0100

    Added quick readme file.

--- IDaaS/IDaaS/conf/local_settings_example.py
+++ IDaaS/IDaaS/conf/local_settings_example.py
@@ -1,23 +1,3 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-DATABASES = {
-    'ldap': {
-        'ENGINE': 'ldapdb.backends.ldap',
-        'NAME': 'ldap://IP',
-        'USER': 'cn=admin,dc=domain,dc=com',
-        'PASSWORD': 'ldapPassword',
-     },
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-BASE_DN_USER = "ou=people,dc=domain,dc=com"
-BASE_DN_GROUP = "ou=groups,dc=domain,dc=com"
-
 # G Oauth2 keys
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = "xxxx.apps.googleusercontent.com"
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "secret"
---
+++ README.md
@@ -0,0 +1,2 @@
+Simple Django app that let's you sync GApps users to Django.
+This can be then used as an authenticating REST backend for freeradius server.
